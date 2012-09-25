41511cc788cd7700a8c363e7431bd0675294f239
Tobias Kuhn <kuhntobias@gmail.com>
2012-09-25 13:50:23 -0400

    Interlink nanopubs and claims; add menu bar

--- src/main/java/ch/tkuhn/nanobrowser/ClaimPage.java
+++ src/main/java/ch/tkuhn/nanobrowser/ClaimPage.java
@@ -1,8 +1,14 @@
 package ch.tkuhn.nanobrowser;
 
+import java.util.List;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.openrdf.query.BindingSet;
 
 public class ClaimPage extends WebPage {
 
@@ -15,6 +21,27 @@ public class ClaimPage extends WebPage {
 		add(new Label("title", Utils.getSentenceFromURI(uri)));
 		
 		add(new Label("uri", uri));
+		
+		String claimQuery = "select distinct ?p where {?p <http://www.nanopub.org/nschema#hasAssertion> ?a . ?a <http://krauthammerlab.med.yale.edu/nanopub/extensions/asSentence> <" + uri + ">}";
+		List<BindingSet> claims = TripleStoreAccess.getTuples(claimQuery);
+		
+		ListView<BindingSet> claimList = new ListView<BindingSet>("nanopubs", claims) {
+			
+			private static final long serialVersionUID = 3911519757128281636L;
+
+			protected void populateItem(ListItem<BindingSet> item) {
+				BindingSet b = item.getModelObject();
+				String uri = b.getValue("p").stringValue();
+				String s = Utils.getLastPartOfURI(uri);
+				PageParameters params = new PageParameters();
+				params.add("uri", uri);
+				BookmarkablePageLink<WebPage> link = new BookmarkablePageLink<WebPage>("nanopublink", PubPage.class, params);
+				item.add(link);
+				link.add(new Label("nanopub", s));
+			}
+			
+		};
+		add(claimList);
 		
 	}
 
--- src/main/java/ch/tkuhn/nanobrowser/PubPage.java
+++ src/main/java/ch/tkuhn/nanobrowser/PubPage.java
@@ -1,8 +1,14 @@
 package ch.tkuhn.nanobrowser;
 
+import java.util.List;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.openrdf.query.BindingSet;
 
 public class PubPage extends WebPage {
 
@@ -15,6 +21,27 @@ public class PubPage extends WebPage {
 		add(new Label("title", Utils.getLastPartOfURI(uri)));
 		
 		add(new Label("uri", uri));
+		
+		String claimQuery = "select distinct ?c where {<" + uri + "> <http://www.nanopub.org/nschema#hasAssertion> ?a . ?a <http://krauthammerlab.med.yale.edu/nanopub/extensions/asSentence> ?c}";
+		List<BindingSet> claims = TripleStoreAccess.getTuples(claimQuery);
+		
+		ListView<BindingSet> claimList = new ListView<BindingSet>("claims", claims) {
+			
+			private static final long serialVersionUID = 3911519757128281636L;
+
+			protected void populateItem(ListItem<BindingSet> item) {
+				BindingSet b = item.getModelObject();
+				String uri = b.getValue("c").stringValue();
+				String s = Utils.getSentenceFromURI(uri);
+				PageParameters params = new PageParameters();
+				params.add("uri", uri);
+				BookmarkablePageLink<WebPage> link = new BookmarkablePageLink<WebPage>("claimlink", ClaimPage.class, params);
+				item.add(link);
+				link.add(new Label("claim", s));
+			}
+			
+		};
+		add(claimList);
 		
 	}
 
